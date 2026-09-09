49b0b1801dcf6a3bd9c161e449cd96e49cbb0d8f
Henrique Nunes <79033028+riquen@users.noreply.github.com>
2026-09-09 10:48:23 -0300

    "chore: configure code formatting"

--- src/mocks/transactions.ts
+++ src/mocks/transactions.ts
@@ -1,12 +1,12 @@
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 
-export const mockTransactions = Array.from({ length: 20 }, () => ({ 
-    id: faker.string.uuid(),
-    description: faker.company.name(),
-    amount: faker.number.float({
-        min: -1000,
-        max: 5000,
-        fractionDigits: 2,
-    }),
-    date: faker.date.recent({ days: 30})
-}))
+export const mockTransactions = Array.from({ length: 20 }, () => ({
+  id: faker.string.uuid(),
+  description: faker.company.name(),
+  amount: faker.number.float({
+    min: -1000,
+    max: 5000,
+    fractionDigits: 2,
+  }),
+  date: faker.date.recent({ days: 30 }),
+}));
