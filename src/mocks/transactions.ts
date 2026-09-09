0acbc932d7cb77b6216d9ea9a0f73f57ac912b61
Henrique Nunes <79033028+riquen@users.noreply.github.com>
2026-09-09 10:24:58 -0300

    chore: initialize BankLab React app

---
+++ src/mocks/transactions.ts
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+
+export const mockTransactions = Array.from({ length: 20 }, () => ({ 
+    id: faker.string.uuid(),
+    description: faker.company.name(),
+    amount: faker.number.float({
+        min: -1000,
+        max: 5000,
+        fractionDigits: 2,
+    }),
+    date: faker.date.recent({ days: 30})
+}))
